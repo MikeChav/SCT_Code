98e051b6e366d81a44bbce4e13efb7b4dc7a0214
David E. Narváez <david.narvaez@computer.org>
2022-08-05 15:05:50 -0400

    Generate Random Veto Examples
    Integrated the veto function and made the election system in the script
    a generic parameter so now the same script can produce plurality or veto
    examples.
    
    Included is the example Michael found.

--- examples.py
+++ examples.py
@@ -14,8 +14,8 @@ class Control:
         self.ties_promote = ties_promote
         self.unique_winner = unique_winner
 
-    def __call__(self, V, C):
-        return self.f(V, C, self.ties_promote, self.unique_winner)
+    def __call__(self, E, V, C):
+        return self.f(E, V, C, self.ties_promote, self.unique_winner)
 
 
 def powerset(iterable):
@@ -35,7 +35,7 @@ def mask(profile, candidates):
     return new_profile
 
 
-def winners(profile, candidates):
+def plurality(profile, candidates):
     if len(candidates) == 0:
         return []
     counts = {c: 0 for c in candidates}
@@ -45,85 +45,97 @@ def winners(profile, candidates):
     return [t for t in candidates if counts[t] == counts[potential_winner]]
 
 
-def subelection(profile, candidates, ties_promote):
-    w = winners(mask(profile, candidates), candidates)
+def veto(profile, candidates):
+    if len(candidates) == 0:
+        return []
+    if len(profile) == 0:
+        return candidates
+    counts = {c: len(profile) for c in candidates}
+    for vote in profile:
+        counts[vote[-1]] -= 1
+    potential_winner = max(counts, key=counts.get)
+    return [t for t in candidates if counts[t] == counts[potential_winner]]
+
+
+def subelection(election_system, profile, candidates, ties_promote):
+    w = election_system(mask(profile, candidates), candidates)
     if not ties_promote and len(w) > 1:
         return []
     return w
 
 
-def finalround(profile, candidates, unique_winner):
-    return subelection(profile, candidates, not unique_winner)
+def finalround(election_system, profile, candidates, unique_winner):
+    return subelection(election_system, profile, candidates, not unique_winner)
 
 
-def vote_partition_winners(candidates, votes, tie, model):
-    '''Computes Plurality_{CC-PV-X-Y}'''
+def vote_partition_winners(election_system, candidates, votes, tie, model):
+    '''Computes E_{CC-PV-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(votes):
-        w1 = subelection(subset1, candidates, tie)
-        w2 = subelection([v for v in votes if v not in subset1], candidates, tie)
-        current = finalround(votes, set(w1) | set(w2), model)
+        w1 = subelection(election_system, subset1, candidates, tie)
+        w2 = subelection(election_system, [v for v in votes if v not in subset1], candidates, tie)
+        current = finalround(election_system, votes, set(w1) | set(w2), model)
         for w in current:
             overall[w].append(subset1)
     return overall
 
 
-def runoff_candidate_partition_winners(candidates, profile, tie, model):
-    '''Computes Plurality_{CC-RPC-X-Y}'''
+def runoff_candidate_partition_winners(election_system, candidates, profile, tie, model):
+    '''Computes E_{CC-RPC-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(candidates):
-        w1 = subelection(profile, subset1, tie)
-        w2 = subelection(profile, [p for p in candidates if p not in subset1], tie)
-        current = finalround(profile, set(w1) | set(w2), model)
+        w1 = subelection(election_system, profile, subset1, tie)
+        w2 = subelection(election_system, profile, [p for p in candidates if p not in subset1], tie)
+        current = finalround(election_system, profile, set(w1) | set(w2), model)
         for w in current:
             overall[w].append(subset1)
     return overall
 
 
-def candidate_partition_winners(candidates, profile, tie, model):
-    '''Computes Plurality_{CC-PC-X-Y}'''
+def candidate_partition_winners(election_system, candidates, profile, tie, model):
+    '''Computes E_{CC-PC-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(candidates):
-        w1 = subelection(profile, subset1, tie)
-        current = finalround(profile, set(w1) | set([p for p in candidates if p not in subset1]), model)
+        w1 = subelection(election_system, profile, subset1, tie)
+        current = finalround(election_system, profile, set(w1) | set([p for p in candidates if p not in subset1]), model)
         for w in current:
             overall[w].append(subset1)
     return overall
 
 
-def runoff_candidate_partition_losers(candidates, votes, tie, uw):
-    '''Computes Plurality_{DC-RPC-X-Y}'''
+def runoff_candidate_partition_losers(election_system, candidates, votes, tie, uw):
+    '''Computes E_{DC-RPC-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(candidates):
-        w1 = subelection(votes, subset1, tie)
-        w2 = subelection(votes, list(set(candidates) - set(subset1)), tie)
-        current = finalround(votes, set(w1) | set(w2), uw)
+        w1 = subelection(election_system, votes, subset1, tie)
+        w2 = subelection(election_system, votes, list(set(candidates) - set(subset1)), tie)
+        current = finalround(election_system, votes, set(w1) | set(w2), uw)
         for c in candidates:
             if c not in current:
                 overall[c].append(subset1)
     return overall
 
 
-def candidate_partition_losers(candidates, votes, tie, uw):
-    '''Computes Plurality_{DC-PC-X-Y}'''
+def candidate_partition_losers(election_system, candidates, votes, tie, uw):
+    '''Computes E_{DC-PC-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(candidates):
-        w1 = subelection(votes, subset1, tie)
-        current = finalround(votes, set(w1) | (set(candidates) - set(subset1)), uw)
+        w1 = subelection(election_system, votes, subset1, tie)
+        current = finalround(election_system, votes, set(w1) | (set(candidates) - set(subset1)), uw)
         for c in candidates:
             if c not in current:
                 overall[c].append(subset1)
     return overall
 
 
-def vote_partition_losers(candidates, votes, tie, uw):
-    '''Computes Plurality_{DC-PV-X-Y}'''
+def vote_partition_losers(election_system, candidates, votes, tie, uw):
+    '''Computes E_{DC-PV-X-Y}'''
     overall = collections.defaultdict(list)
     for subset1 in powerset(votes):
-        w1 = subelection(subset1, candidates, tie)
+        w1 = subelection(election_system, subset1, candidates, tie)
         subset2 = [v for v in votes if v not in subset1]
-        w2 = subelection(subset2, candidates, tie)
-        current = finalround(votes, set(w1) | set(w2), uw)
+        w2 = subelection(election_system, subset2, candidates, tie)
+        current = finalround(election_system, votes, set(w1) | set(w2), uw)
         for c in candidates:
             if c not in current:
                 if subset1 not in overall[c]:
@@ -176,9 +188,10 @@ def parse_function(cname):
     return Control(f, TP, UW)
 
 
-def search_example(ncandidates, inset, notinset, N, seed):
+def search_example(election_system, ncandidates, inset, notinset, N, seed):
     random.seed(seed)
 
+    E = plurality if election_system == "plurality" else veto
     C = [chr(ord('a') + x) for x in range(ncandidates)]
     all_votes = sorted(itertools.permutations(C))
     rounds = 0
@@ -190,9 +203,9 @@ def search_example(ncandidates, inset, notinset, N, seed):
             print("round=", rounds, file=sys.stderr)
         rounds += 1
         N = round(random.uniform(0.25, .75) * len(all_votes)) if N is None else N
-        V = [random.choice(all_votes) for i in range(N)] 
-        insetresults = [f(C, V) for f in finset]
-        notinsetresults = [f(C, V) for f in fnotinset]
+        V = [random.choice(all_votes) for i in range(N)]
+        insetresults = [f(E, C, V) for f in finset]
+        notinsetresults = [f(E, C, V) for f in fnotinset]
         insetint = set(C)
         notinsetun = set()
 
@@ -210,6 +223,7 @@ def search_example(ncandidates, inset, notinset, N, seed):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find examples of plurality control instances that are in some sets but not in others")
+    parser.add_argument("election_system", choices=["plurality", "veto"], help="Election system")
     parser.add_argument("ncandidates", type=int, help="Number of candidates")
     parser.add_argument("--votes", "-N", type=int, help="Number of random votes (picked with replacement)")
     parser.add_argument("--seed", "-s", type=int, help="Random seed")
@@ -224,7 +238,7 @@ if __name__ == "__main__":
         print(seed)
 
     start = time.process_time()
-    res = search_example(opt.ncandidates, opt.inset, opt.notinset, opt.votes, seed)
+    res = search_example(opt.election_system, opt.ncandidates, opt.inset, opt.notinset, opt.votes, seed)
     elapsed = time.process_time() - start
 
     if res is not None:
@@ -235,4 +249,3 @@ if __name__ == "__main__":
         print(json.dumps({"C": opt.ncandidates, "V": V, "version": "1.0", "date": datetime.datetime.utcnow().ctime(), "elapsedTime": elapsed}))
     else:
         sys.exit(1)
-
